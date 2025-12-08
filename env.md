58153d8a9476489fdf722ecd4fa08709a6103048
Cosmo-klara <wincosmo9@outlook.com>
2025-12-08 23:22:49 +0800

    new file:   .gitignore 	new file:   env.md 	new file:   vit_r/test.ipynb 	new file:   vit_r/test.py

---
+++ env.md
@@ -0,0 +1,13 @@
+
+```zsh
+conda create -n qwen_lora python=3.10
+conda activate qwen_lora
+pip install transformers
+pip install ipython ipykernel
+pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install modelscope
+pip install av==14.3.0
+pip install qwen-omni-utils==0.0.4
+pip install accelerate
+pip install flash_attn-2.7.1.post4+cu11torch2.6cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
+```
---
+++ vit_r/test.py
@@ -0,0 +1,116 @@
+import os
+os.environ['HTTP_PROXY'] = 'http://127.0.0.1:7890'
+os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:7890'
+os.environ["CUDA_VISIBLE_DEVICES"] = "2,3"
+
+import torch
+from datasets import load_dataset
+from transformers import AutoImageProcessor, AutoModelForImageClassification, TrainingArguments, Trainer
+from peft import LoraConfig, get_peft_model
+from torchvision.transforms import (
+    CenterCrop,
+    Compose,
+    Normalize,
+    RandomHorizontalFlip,
+    RandomResizedCrop,
+    Resize,
+    ToTensor,
+)
+
+
+ds = load_dataset("food101")
+
+labels = ds["train"].features["label"].names
+label2id, id2label = dict(), dict()
+for i, label in enumerate(labels):
+    label2id[label] = i
+    id2label[i] = label
+
+image_processor = AutoImageProcessor.from_pretrained(
+    "google/vit-base-patch16-224-in21k",
+    use_fast=True, # 修改，有更快的预处理函数
+)
+
+normalize = Normalize(mean=image_processor.image_mean, std=image_processor.image_std)
+train_transforms = Compose(
+    [
+        RandomResizedCrop(image_processor.size["height"]),
+        RandomHorizontalFlip(),
+        ToTensor(),
+        normalize,
+    ]
+)
+
+val_transforms = Compose(
+    [
+        Resize(image_processor.size["height"]),
+        CenterCrop(image_processor.size["height"]),
+        ToTensor(),
+        normalize,
+    ]
+)
+
+def preprocess_train(example_batch):
+    example_batch["pixel_values"] = [train_transforms(image.convert("RGB")) for image in example_batch["image"]]
+    return example_batch
+
+def preprocess_val(example_batch):
+    example_batch["pixel_values"] = [val_transforms(image.convert("RGB")) for image in example_batch["image"]]
+    return example_batch
+    
+train_ds = ds["train"]
+val_ds = ds["validation"]
+
+train_ds.set_transform(preprocess_train)
+val_ds.set_transform(preprocess_val)
+
+def collate_fn(examples):
+    pixel_values = torch.stack([example["pixel_values"] for example in examples])
+    labels = torch.tensor([example["label"] for example in examples])
+    return {"pixel_values": pixel_values, "labels": labels}
+
+model = AutoModelForImageClassification.from_pretrained(
+    "google/vit-base-patch16-224-in21k",
+    label2id=label2id,
+    id2label=id2label,
+    ignore_mismatched_sizes=True,
+)
+
+config = LoraConfig(
+    r=16,
+    lora_alpha=16,
+    target_modules=["query", "value"],
+    lora_dropout=0.1,
+    bias="none",
+    modules_to_save=["classifier"],
+)
+model = get_peft_model(model, config)
+model.print_trainable_parameters()
+
+batch_size = 128
+
+args = TrainingArguments(
+    output_dir="./r_models",
+    remove_unused_columns=False,
+    eval_strategy="epoch",
+    save_strategy="epoch",
+    learning_rate=5e-3,
+    per_device_train_batch_size=batch_size,
+    gradient_accumulation_steps=4,
+    per_device_eval_batch_size=batch_size,
+    fp16=True,
+    num_train_epochs=2, # 5 -> 2
+    logging_steps=10,
+    load_best_model_at_end=True,
+    label_names=["labels"],
+)
+
+trainer = Trainer(
+    model,
+    args,
+    train_dataset=train_ds,
+    eval_dataset=val_ds,
+    processing_class=image_processor,
+    data_collator=collate_fn,
+)
+trainer.train()
